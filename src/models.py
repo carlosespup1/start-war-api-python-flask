5bca541424e5ec5c075ee05ba1bdc2990ab37b75
Carlos Espinoza <obandocarlos604@gmail.com>
2021-04-11 02:45:52 +0000

    first commit

--- src/models.py
+++ src/models.py
@@ -1,9 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean, Float
 
 db = SQLAlchemy()
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
@@ -13,7 +14,25 @@ class User(db.Model):
 
     def serialize(self):
         return {
-            "id": self.id,
+            "userId": self.userId,
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Characters(db.Model):
+    idCharacter = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250))
+    weight = db.Column(db.Integer)
+    height = db.Column(db.Integer)
+
+class Planets(db.Model):
+    idPlanet = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(40), nullable=False)
+    Population = db.Column(db.Integer)
+    sizeKm = db.Column(db.Float)
+
+class Favorities(db.Model):
+    favoriteId = db.Column(db.Integer, primary_key=True)
+    userId = db.Column(ForeignKey(User.userId))
+    planetId = db.Column(ForeignKey(Planets.idPlanet))
+    characterId = db.Column(ForeignKey(Characters.idCharacter))
